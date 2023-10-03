57e3df37871c4cea3a78cf8e7ae79285ec46fcb3
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-10-03 11:56:04 -0500

    class 7 - implementing Login with PassportJS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
-const {checkApiKey} = require('./middlewares/auth.handler')
+const { checkApiKey } = require('./middlewares/auth.handler');
 
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
@@ -21,6 +21,8 @@ const options = {
   }
 }
 app.use(cors(options));
+
+require('./utils/auth');
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
