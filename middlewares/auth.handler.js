1427f8f4a0b4cc08f310acb774472e64177be26c
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-10-03 14:49:53 -0500

    class 12 - mid of the class, V1 with checkadminrole function, it will changed

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -11,4 +11,14 @@ function checkApiKey(req, res, next) {
   }
 }
 
-module.exports = { checkApiKey }
+function checkAdminRole(req, res, next) {
+  console.log(req.user)
+  const user = req.user;
+  if(user.role === 'admin'){
+    next();
+  } else {
+    next(boom.unauthorized());
+  }
+}
+
+module.exports = { checkApiKey, checkAdminRole }
